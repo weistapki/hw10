6149c81cdd31d78a8f53c4a7697ccce337b633b4
weistapki <weistapki@gmail.com>
2023-04-14 15:12:30 +0200

    added method getLastThreeAddedProducts

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,11 +9,11 @@ import static java.util.stream.Collectors.toList;
 public class Main {
     public static void main(String[] args) {
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product("Book", 350.0, true));
-        productList.add(new Product("Book", 180.0, false));
-        productList.add(new Product("Book", 70.0, true));
-        productList.add(new Product("Electronics", 520.0, false));
-        productList.add(new Product("Clothing", 60.0, true));
+        productList.add(new Product("Book", 350.0, true, LocalDate.of(2023, 3, 15)));
+        productList.add(new Product("Book", 180.0, false, LocalDate.of(2022, 6, 10)));
+        productList.add(new Product("Book", 70.0, true, LocalDate.of(2023, 4, 5)));
+        productList.add(new Product("Electronics", 520.0, false, LocalDate.of(2023, 2, 20)));
+        productList.add(new Product("Clothing", 60.0, true, LocalDate.of(2023, 1, 10)));
 
 
         System.out.println("The total value of eligible products whose category:");
@@ -23,6 +24,9 @@ public class Main {
         System.out.println();
         System.out.println("The cheapest product from the category \"Book\":");
         System.out.println(getCheapestBook(productList));
+        System.out.println();
+        System.out.println("Get the last three products added:");
+        System.out.println(getLastThreeAddedProducts(productList));
     }
     public static List<Product> getExpensiveBooks(List<Product> productList) {
         return productList.stream()
@@ -38,11 +42,18 @@ public class Main {
                     p.setPrice(p.getPrice() * 0.9);// Применение скидки 10%
                 })
                 .collect(toList());
-    };
+    }
+
     public static Product getCheapestBook(List<Product> products) {
         return products.stream()
                 .filter(p -> p.getType().equals("Book"))
                 .min(Comparator.comparingDouble(Product::getPrice))
                 .orElseThrow(() -> new RuntimeException("Product [Category: Book] not found"));
+    }
+    public static List<Product> getLastThreeAddedProducts(List<Product> productList) {
+        return productList.stream()
+                .sorted(Comparator.comparing(Product::getDateAdded).reversed()) // Сортировка по дате в обратном порядке
+                .limit(3) // Ограничение до трех элементов
+                .toList();
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,16 @@
+import java.time.LocalDate;
+
 public class Product {
     private String type;
     private double price;
     private boolean discount;
+    private LocalDate dateAdded;
 
-    public Product(String type, double price, boolean discount) {
+    public Product(String type, double price, boolean discount, LocalDate dateAdded) {
         this.type = type;
         this.price = price;
         this.discount = discount;
+        this.dateAdded = dateAdded;
     }
 
     public String getType() {
@@ -23,12 +27,18 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
                 "type='" + type + '\'' +
                 ", price=" + price +
                 ", discount=" + discount +
+                ", dateAdded=" + dateAdded +
                 '}';
     }
 }
